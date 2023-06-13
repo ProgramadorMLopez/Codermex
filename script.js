4f375c52ce81120e6f9f44b5de792d95a94712ec
MLopez <programadormlopez@gmail.com>
2023-06-13 09:50:52 -0300

    Se agrego un script para el evento de scroll y le agregamos una translate

--- script.js
+++ script.js
@@ -7,4 +7,20 @@ window.onfocus = () =>
 /*********************************/
 
 
+window.addEventListener('load', function () {
+	var Clase = document.querySelector(".container-main");
+
+	Clase.addEventListener('scroll', function (event) {
+		event.preventDefault();
+		console.log("Evento submit capturado");
+		// Todo tu codigo va acá
+		var SubClase = document.getElementsByClassName("title");
+		for (var i = 0; i < SubClase.length; i++) {
+			SubClase[i].classList.add("mov-title");
+			SubClase[i].classList.remove("title");
+
+		}
+	});
+});
+
 
