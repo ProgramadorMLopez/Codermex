435b392b8c153eb0c11ded148036e43085ead19b
MLopez <programadormlopez@gmail.com>
2023-06-24 20:27:58 -0300

    arreglo web principal

--- script.js
+++ script.js
@@ -1,35 +1,18 @@
+//Al salir de la web te muestra otro titulo
 let baslik = document.title;
 window.onblur = () =>
 	document.title = "Te estamos esperando :)";
 window.onfocus = () =>
 	document.title = "Codermex";
 
-/*********************************/
 
+const nav = document.querySelector('nav');
 
-window.addEventListener('load', function () {
-	var Clase = document.querySelector(".container-main");
-
-	Clase.addEventListener('scroll', function (event) {
-		event.preventDefault();
-		//console.log("Evento submit capturado");
-
-		// Todo tu codigo va acá
-		var SubClase = document.getElementsByClassName("title");
-		for (var i = 0; i < SubClase.length; i++) {
-			SubClase[i].classList.add("mov-title");
-			SubClase[i].classList.remove("title");
-
-		}
-		var SubClase = document.getElementsByClassName("navbar");
-		for (var i = 0; i < SubClase.length; i++) {
-			SubClase[i].classList.add("navbar-color-1");
-		}
-		var SubClase = document.getElementsByClassName("navbar-nav");
-		for (var i = 0; i < SubClase.length; i++) {
-			SubClase[i].classList.add("navbar-color-2");
-		}
-
-	});
+window.addEventListener('scroll', function () {
+	if (window.scrollY > 0) {
+		/*nav.classList.remove('navbar-color-1');*/
+		nav.classList.add('navbar-color-2');
+	} else {
+		nav.classList.remove('navbar-color-2');
+	}
 });
-
